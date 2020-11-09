ee85d99d63e34a18cc71f8d817f24ab11ac3d735
changrui <changr999@163.com>
2020-11-09 22:29:54 +0800

    task: 01-01

---
+++ code/exercises_04.js
@@ -0,0 +1,8 @@
+// 四、手写实现MyPromise源码
+
+// 要求：尽可能还原Promise中的每一个API，并通过注释的方式描述思路和原理
+
+// 还木有写完~~~老师可不可以先帮我解锁模块二  追赶中~~~
+const PEDDING = 'pedding' //等待
+const FUFILLED = 'fufilled' //成功
+const REJECT = 'reject' //失败
